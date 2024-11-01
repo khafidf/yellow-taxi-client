ecb88181ce06fed1b6a79a2c8204eecfd17bce50
khafidf <khfd027@gmail.com>
2024-11-01 19:06:16 +0700

    new config

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_BASE_URL,
+		baseUrl: "https://yellow-taxi-server.vercel.app/",
 	}),
 	endpoints: (builder) => ({}),
 });
